5a675aeb7624b9167c4459929947991047df9832
RayanDiniz <rayancassiokiol@gmail.com>
2020-12-28 14:44:39 -0300

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-    Button,
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-    Media
+    Container, Row, Col, Button, Form, FormGroup, Input
 } from 'reactstrap';
 
-export default function Home() {
-    const [isOpen, setIsOpen] = useState(false);
+import Image from 'next/image';
+import Head from 'next/head';
 
-    const toggle = () => setIsOpen(!isOpen);
+export default function Home() {
 
     return (
-        <>
-            <Navbar color="primary" dark expand="md">
-                <NavbarBrand href="/">Celke</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="https://celke.com.br/">Site Celke</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                Options
-                </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>
-                                    Option 1
-                  </DropdownItem>
-                                <DropdownItem>
-                                    Option 2
-                  </DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>
-                                    Reset
-                  </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <NavbarText>Simple Text</NavbarText>
-                </Collapse>
-            </Navbar>
-
-            
-            <Media left href="#">
-                <Media object data-src="https://th.bing.com/th/id/OIP.q9gSgF4Aa0uHsFTTLWuv7AHaEK?pid=Api&rs=1" alt="Generic placeholder image" />
-            </Media>
-            <h1>Home</h1>
-            
-            <Button color="danger">Danger!</Button>
-        </>
+        <div className="body">
+            <Head>
+                <title>Projeto</title>
+                <link href="/src/style.css" rel="stylesheet" />
+            </Head>
+            <Container>
+                <Container>
+                <h3>Free Ebook: The Complete Guide to Facebook Ads</h3>
+                <hr color="white" height="3px"></hr>
+                <Row>
+                    <Col xs="6">
+                        <p><b>Want a crash course in everything you need to know about Facebook Ads?</b></p>
+                        <p>This 82-page ebook will teach you everything, from budgeting and design to advanced targeting, strateges to get the most bang for you buck, and much more!</p>
+                        <Image
+                            src="/src/img/logo.png"
+                            width={473}
+                            height={329}
+                        />
+                    </Col>
+                    <Col xs="6">
+                        <div className="form">
+                        <h4>Download Your Free Guide Today!</h4>
+                        <Form className="formu">
+                            <FormGroup>
+                                <Input type="email" name="email" placeholder="Your First Name" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="email" name="password" placeholder="Your Email" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" name="site"  placeholder="Your Business Website" />
+                            </FormGroup>
+                            <Button color="warning" className="text-white"><b>Get My FREE Ebook</b></Button>
+                        </Form>
+                        </div>
+                    </Col>
+                </Row>
+                </Container>
+            </Container>
+        </div>
     );
 };
